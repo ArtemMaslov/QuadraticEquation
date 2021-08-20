3237c25cc753a1889c28b2c4a3fc96a01c10ae34
ArtemMaslov <artem.m198@gmail.com>
2021-08-20 09:11:22 +0300

    Release added

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 ﻿#include <Windows.h>
 
-#include "..\inc\euation_client.h"
-#include "..\inc\test.h"
+#include "..\inc\equation_client.h"
+//#include "..\inc\test.h"
 
 int main(void)
 {
     SetConsoleCP(1251);       // установка кодовой страницы win-cp 1251 в поток ввода
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 
-    //EquationClient();
-    TestSolveTask();
+    EquationClient();
+    //TestSolveTask();
     return 0;
 }
